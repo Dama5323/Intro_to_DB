04bc7601d6b9bfbacba6f00a6ca3577ea72d9ae4
Dama5323 <your-deenyashke@gmail.com>
2025-06-28 02:21:41 +0300

    Write a script that prints the full description of the table books from the database alx_book_store in your MySQL server.

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,13 @@
--- Prints full description of books table without DESCRIBE/EXPLAIN
+-- Full description of books table without using DESCRIBE/EXPLAIN/ANALYZE
 SELECT 
     COLUMN_NAME AS 'Field',
     COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
+    IF(IS_NULLABLE = 'YES', 'YES', 'NO') AS 'Null',
+    IFNULL(COLUMN_KEY, '') AS 'Key',
+    IFNULL(COLUMN_DEFAULT, 'NULL') AS 'Default',
+    IFNULL(EXTRA, '') AS 'Extra'
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = DATABASE()
+    TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'books';
