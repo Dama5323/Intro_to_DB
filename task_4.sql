067ff15c0bd739d1b7d2d305ac30b2007736c90b
Dama5323 <your-deenyashke@gmail.com>
2025-06-28 02:26:43 +0300

    Inserts a single customer record

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,15 @@
 -- Full description of books table without using DESCRIBE/EXPLAIN/ANALYZE
 SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IF(IS_NULLABLE = 'YES', 'YES', 'NO') AS 'Null',
-    IFNULL(COLUMN_KEY, '') AS 'Key',
-    IFNULL(COLUMN_DEFAULT, 'NULL') AS 'Default',
-    IFNULL(EXTRA, '') AS 'Extra'
+    TABLE_NAME,
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
     TABLE_SCHEMA = 'alx_book_store'
-    AND TABLE_NAME = 'books';
+    AND TABLE_NAME = 'Books';
+
