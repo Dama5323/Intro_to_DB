d02c0d8cb77991919023118673cf8d740682f400
Dama5323 <your-deenyashke@gmail.com>
2025-06-28 01:54:06 +0300

    Writing a script that creates the database alx_book_store in  MySQL server.

---
+++ task_2.sql
@@ -0,0 +1,40 @@
+-- Creates all tables for the alx_book_store database
+-- task_2.sql
+USE alx_book_store;
+
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES Books(book_id)
+) ENGINE=InnoDB;
